c4b5aa27fb2806a64bc257cdc550507451c742ac
ndo <ndo@amazon.com>
2023-06-06 13:51:54 -0700

    Add rounds 1-4 results, comments

--- main.py
+++ main.py
@@ -44,4 +44,39 @@ page = scrape_site(URL)
 player_data_list = create_player_data_list(page)
 player_dict = create_player_objects(player_data_list)
 
+print(player_dict["Mikal Bridges"])
 print(player_dict["Stephen Curry"])
+
+"""
+Example output: 
+
+{'name': 'Stephen Curry', 'pos': 'PG', 'age': '34', 'team': 'GSW', 'g': '56', 'gs': '56', 
+'mp': '1941', 'fg': '559', 'fga': '1133', 'fg%': '.493', '3p': '273', '3pa': '639', 
+'3p%': '.427', '2p': '286', '2pa': '494', '2p%': '.579', 'efg%': '.614', 'ft': '257', 
+'fta': '281', 'ft%': '.915', 'orb': '39', 'drb': '302', 'trb': '341', 'ast': '352', 
+'stl': '52', 'blk': '20', 'tov': '179', 'pf': '117', 'pts': '1648'}
+
+For 8-cat h2h, the only categories I care about are:
+
+FG%
+FT%
+3PM
+Rebounds
+Assists
+Steals
+Blocks
+Points
+
+I'd like to create some sort of framework where I give players a score based on the above. I'd also like to figure out what combination of the above
+is the most effective.
+
+How can I factor machine learning into the above?
+
+I need to quantify
+
+
+I could maybe compete against auto-drafted teams in a head-2-head based on the whole season and see who would've won?
+If I do that, I need to see who would've been auto-drafted in each year going back. Probably not going to be super easy.
+It might be easier to just create some sort of ranking system for each player.
+
+"""
